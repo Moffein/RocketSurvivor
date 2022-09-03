0773cd496055d88c1438b30429accd590ea4b2a9
Moffein <22742925+Moffein@users.noreply.github.com>
2022-09-03 12:42:00 -0700

    Changed language tokens so everything is called "Rockets" instead of "Missiles"

--- HenryMod/Modules/Tokens.cs
+++ HenryMod/Modules/Tokens.cs
@@ -17,15 +17,15 @@ namespace RocketSurvivor.Modules
             LanguageAPI.Add(prefix + "PASSIVE_NAME", "Blastoff");
             LanguageAPI.Add(prefix + "PASSIVE_DESCRIPTION", "Your explosives can be used to <style=cIsUtility>blast jump</style>.");
 
-            LanguageAPI.Add(prefix + "PRIMARY_NAME", "HG4 Missile Launcher");
-            LanguageAPI.Add(prefix + "PRIMARY_DESCRIPTION", "<style=cIsUtility>Agile</style>. Fire a missile for <style=cIsDamage>520% damage</style>. Can hold up to 4.");
+            LanguageAPI.Add(prefix + "PRIMARY_NAME", "HG4 Rocket Launcher");
+            LanguageAPI.Add(prefix + "PRIMARY_DESCRIPTION", "<style=cIsUtility>Agile</style>. Fire a rocket for <style=cIsDamage>520% damage</style>. Can hold up to 4.");
 
             //Unlock Condition: Hit 20 airshots.
-            LanguageAPI.Add(prefix + "PRIMARY_ALT_NAME", "HG4 SAM Launcher");
-            LanguageAPI.Add(prefix + "PRIMARY_ALT_DESCRIPTION", "<style=cIsUtility>Agile</style>. Fire a precision missile for <style=cIsDamage>600% damage</style>. Deals <style=cIsDamage>+30% damage</style> against <style=cIsUtility>airborne targets</style>. Can hold up to 4.");
+            LanguageAPI.Add(prefix + "PRIMARY_ALT_NAME", "HG7 SAM Launcher");
+            LanguageAPI.Add(prefix + "PRIMARY_ALT_DESCRIPTION", "<style=cIsUtility>Agile</style>. Fire a precision rocket for <style=cIsDamage>600% damage</style>. Deals <style=cIsDamage>+30% damage</style> against <style=cIsUtility>airborne targets</style>. Can hold up to 4.");
 
             LanguageAPI.Add(prefix + "SECONDARY_NAME", "Remote Detonator");
-            LanguageAPI.Add(prefix + "SECONDARY_DESCRIPTION", "Detonate your <style=cIsDamage>last-fired missile</style>, increasing its <style=cIsDamage>damage</style> and <style=cIsDamage>blast radius</style> by <style=cIsDamage>30%</style>.");
+            LanguageAPI.Add(prefix + "SECONDARY_DESCRIPTION", "Detonate your <style=cIsDamage>last-fired rocket</style>, increasing its <style=cIsDamage>damage</style> and <style=cIsDamage>blast radius</style> by <style=cIsDamage>30%</style>.");
 
             LanguageAPI.Add(prefix + "UTILITY_NAME", "Concussion Blast");
             LanguageAPI.Add(prefix + "UTILITY_DESCRIPTION", "<style=cIsDamage>Stunning</style>. Toss a nonlethal grenade that <style=cIsUtility>pushes</style> enemies away and reduces their <style=cIsDamage>armor</style> by <style=cIsDamage>40</style> while <style=cIsUtility>airborne</style>.");
@@ -35,7 +35,7 @@ namespace RocketSurvivor.Modules
             LanguageAPI.Add(prefix + "UTILITY_ALT_DESCRIPTION", "<style=cIsDamage>Stunning</style>. Swing an explosive at an enemy for <style=cIsDamage>1000% damage</style>. Deals <style=cIsDamage>Critical Strikes</style> while <style=cIsUtility>rocket jumping</style>.");
 
             LanguageAPI.Add(prefix + "SPECIAL_NAME", "Rapid Rearm");
-            LanguageAPI.Add(prefix + "SPECIAL_DESCRIPTION", "Rapidly <style=cIsDamage>fire 4 missiles</style>, then <style=cIsUtility>reload your weapon</style>.");
+            LanguageAPI.Add(prefix + "SPECIAL_DESCRIPTION", "Rapidly <style=cIsDamage>fire 4 rockets</style>, then <style=cIsUtility>reload your weapon</style>.");
         }
     }
 }
