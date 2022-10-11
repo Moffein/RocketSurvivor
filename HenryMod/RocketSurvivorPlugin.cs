0ebb4291bbb4eae12a519558097f25cbaf56951f
Moffein <22742925+Moffein@users.noreply.github.com>
2022-10-10 21:22:53 -0700

    Fixed outro tokens.

--- HenryMod/RocketSurvivorPlugin.cs
+++ HenryMod/RocketSurvivorPlugin.cs
@@ -34,7 +34,7 @@ namespace RocketSurvivor
     {
         public const string MODUID = "com.EnforcerGang.RocketSurvivor";
         public const string MODNAME = "RocketSurvivor";
-        public const string MODVERSION = "0.2.15";
+        public const string MODVERSION = "0.2.16";
 
         // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
         public const string DEVELOPER_PREFIX = "MOFFEIN";
