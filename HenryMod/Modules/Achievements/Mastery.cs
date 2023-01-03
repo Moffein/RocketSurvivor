24d0ea756ae44d5b504a30d865f10993494ab7bb
Moffein <22742925+Moffein@users.noreply.github.com>
2023-01-03 12:32:06 -0800

    Added Mastery achievement (skin + unlockabledef currently unimplemented).

---
+++ HenryMod/Modules/Achievements/Mastery.cs
@@ -0,0 +1,15 @@
+﻿using RoR2;
+using RoR2.Achievements;
+using UnityEngine;
+
+namespace RocketSurvivor.Modules.Achievements
+{
+    [RegisterAchievement("MoffeinRocketClearGameMonsoon", "Skins.MoffeinRocketSurvivor.Mastery", null, null)]
+    public class Mastery : BasePerSurvivorClearGameMonsoonAchievement
+    {
+        public override BodyIndex LookUpRequiredBodyIndex()
+        {
+            return BodyCatalog.FindBodyIndex("RocketSurvivorBody");
+        }
+    }
+}
