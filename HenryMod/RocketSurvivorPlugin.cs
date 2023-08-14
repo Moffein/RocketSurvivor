2adf3d89918ff0e2749d1f87dd8444185322deda
Moffein <22742925+Moffein@users.noreply.github.com>
2023-08-13 20:57:20 -0700

    Updated version number.

--- HenryMod/RocketSurvivorPlugin.cs
+++ HenryMod/RocketSurvivorPlugin.cs
@@ -35,7 +35,7 @@ namespace RocketSurvivor
     {
         public const string MODUID = "com.EnforcerGang.RocketSurvivor";
         public const string MODNAME = "RocketSurvivor";
-        public const string MODVERSION = "0.8.4";
+        public const string MODVERSION = "0.8.5";
 
         // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
         public const string DEVELOPER_PREFIX = "MOFFEIN";
